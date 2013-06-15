103b68b45f666db6f35419dec1351f6c61216115
Andrew Smith <andrew.smith@intelitycorp.com>
2013-06-15 00:07:35 -0400

    Initial Commit

---
+++ FitFriendsTests/FitFriendsTests.h
@@ -0,0 +1,13 @@
+//
+//  FitFriendsTests.h
+//  FitFriendsTests
+//
+//  Created by Andrew Smith on 2013-06-15.
+//  Copyright (c) 2013 cleverCode. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FitFriendsTests : SenTestCase
+
+@end
---
+++ FitFriendsTests/FitFriendsTests.m
@@ -0,0 +1,32 @@
+//
+//  FitFriendsTests.m
+//  FitFriendsTests
+//
+//  Created by Andrew Smith on 2013-06-15.
+//  Copyright (c) 2013 cleverCode. All rights reserved.
+//
+
+#import "FitFriendsTests.h"
+
+@implementation FitFriendsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FitFriendsTests");
+}
+
+@end
